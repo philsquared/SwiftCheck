74db1801323e587dca24a8db89a0edddb1be135c
Phil Nash <github@philnash.me>
2014-06-03 18:12:16 +0100

    Some actual source code - of sorts

---
+++ SwiftCheckTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCheck
+//
+//  Created by Phil Nash on 03/06/2014.
+//  Copyright (c) 2014 Phil Nash. All rights reserved.
+//
+
+import Foundation
+
+println("This is a placeholder for the real code")
+
